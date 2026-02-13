1c3d4c03c3349a30cb8e23b59c6e7406b4b3c8fd
Tanishk-0x <tanishknamdev981@gmail.com>
2026-02-13 13:11:16 +0530

    fetch

---
+++ src/fetchReq.ts
@@ -0,0 +1,23 @@
+interface Todo {
+    userId : number ; 
+    id : number ; 
+    title : string ;
+    completed : boolean ;
+}
+
+const fetchData = async () => {
+    try {
+        const response = 
+        await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        // Manually Handle 
+        if(!response.ok){
+            throw new Error(`Http erro ${response.status}`);
+        }
+
+        const data: Todo = await response.json() ;
+    }
+    
+    catch (error: any) {
+        
+    }
+}
